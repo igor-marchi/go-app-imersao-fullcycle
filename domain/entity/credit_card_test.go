c61bee98a7eac7876e078ff4681689763ad14216
igor-marchi <igoraugustomarchi@gmail.com>
2021-12-08 23:12:16 -0300

    fix

--- domain/entity/credit_card_test.go
+++ domain/entity/credit_card_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreaditCardNumber(t *testing.T) {
+func TestCreditCardNumber(t *testing.T) {
 	_, err := NewCreditCard("40000000000000", "Jose da Silver", 12, 2024, 123)
 
 	assert.Equal(t, "invalid credit card number", err.Error())
